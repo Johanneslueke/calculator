c775cf83bc3337ed1d5d42114dfcc24bcb1b72b9
lueke.johannes@gmail.com <lueke.johannes@gmail.com>
2018-03-11 15:10:10 +0100

    added doc strings

--- RDP_Arithmetic/ArithmeticRDP/Parser.cs
+++ RDP_Arithmetic/ArithmeticRDP/Parser.cs
@@ -1,27 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Text;
+
 
 
 namespace Calculater_eXtreme
 {
-    //Recursive Descent Parser for Arithmetic Expressions:
-    //
-    // Expression := [ "-" ] Term { ("+" | "-") Term }
-    // Term       := Factor> { ( "*" | "/" | "^" ) Factor }
-    // Factor     := RealNumber | "(" Expression ")"
-    // RealNumber := Digit{Digit} | [Digit] "," {Digit}
-    // Symbol     := Letter | Letter
-    // Digit      := "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" 
-    // Letter     := "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" |
-    //               "L" | "M" | "N"| "O"  | "P" | "Q" | "R" | "S" | "T" | "U"| "V" |
-    //               "W" | "X" | "Y" | "Z" |
-    //               "a" | "b" | "c" | "d" | "e" | "f" | "g"| "h" | "i" | "j" | "k" |
-    //               "l" | "l" | "n"| "o"  | "p" | "q" | "r" | "s" | "t" | "u"| "v" |
-    //               "w" | "x" | "y" | "z" 
-
-
+    /// <summary>
+    /// 
+    ///Recursive Descent Parser for Arithmetic Expressions:
+    ///
+    /// Expression := [ "-" ] Term { ("+" | "-") Term }
+    /// Term       := Factor> { ( "*" | "/" | "^" ) Factor }
+    /// Factor     := RealNumber | "(" Expression ")"
+    /// RealNumber := Digit{Digit} | [Digit] "," {Digit}
+    /// Symbol     := Letter | Letter,Digit | "(" Expression ")"
+    /// Digit      := "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" 
+    ///
+    ///               "L" | "M" | "N"| "O"  | "P" | "Q" | "R" | "S" | "T" | "U"| "V" |
+    ///               "W" | "X" | "Y" | "Z" |
+    ///               "a" | "b" | "c" | "d" | "e" | "f" | "g"| "h" | "i" | "j" | "k" |
+    ///               "l" | "l" | "n"| "o"  | "p" | "q" | "r" | "s" | "t" | "u"| "v" |
+    ///               "w" | "x" | "y" | "z"  
+    /// </summary>
     public class Parser
     {
         private string expression;
@@ -30,6 +31,9 @@ namespace Calculater_eXtreme
         private readonly SymbolTable SymTable = new SymbolTable();
         private readonly RuleTable Rule = new RuleTable();
        
+        /// <summary>
+        /// 
+        /// </summary>
         public String ast
         {
             get
@@ -38,10 +42,16 @@ namespace Calculater_eXtreme
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public IDictionary<String,object> GrammarRules {
             get => (IDictionary<String, object>)Rule.RawTable;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         private void createAST()
         {
             List<Token> flattenAST = (List<Token>)TokenStream.Stream;
@@ -87,6 +97,10 @@ namespace Calculater_eXtreme
             AST.Append("}\n");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expression"></param>
         public Parser(string expression)
         {
             this.expression = expression;
@@ -235,6 +249,10 @@ namespace Calculater_eXtreme
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public double EvaluateExpression()
         {
             var res = Rule["Expression"]();
@@ -243,6 +261,11 @@ namespace Calculater_eXtreme
             return 0;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expr"></param>
+        /// <returns></returns>
         public double EvaluateExpression(string expr)
         {
              expression = expr;
@@ -253,21 +276,37 @@ namespace Calculater_eXtreme
             return 0;
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private bool NextIsMinus()
         {
             return TokenStream.TokensAvailable && TokenStream.IsNextOfType(typeof(MinusToken));
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private bool NextIsOpeningBracket()
         {
             return NextIs(typeof(OpenParenthesisToken));
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private bool NextIsClosingBracket()
         {
             return NextIs(typeof(ClosedParenthesisToken));
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private bool NextIsDigit()
         {
             if (!TokenStream.TokensAvailable)
@@ -275,12 +314,21 @@ namespace Calculater_eXtreme
             return TokenStream.PeekNext() is NumberToken;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private bool NextIsLetter()
         {
             if (!TokenStream.TokensAvailable)
                 return false;
             return TokenStream.PeekNext() is LetterToken;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private bool NextIsSymbol()
         {
             if (!TokenStream.TokensAvailable)
@@ -288,21 +336,33 @@ namespace Calculater_eXtreme
             return TokenStream.PeekNext() is SymbolToken;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
         private bool NextIs(Type type)
         {
             return TokenStream.TokensAvailable && TokenStream.IsNextOfType(type);
         }
 
-
-        //Because in math we calc division and multiplication before any summation
-        //we need to distinguish between those two classes of operation so we might
-        //be able to suspend a summation so multiplication or division can be done
-        //first
+        /// <summary>
+        /// 
+        /// Because in math we calc division and multiplication before any summation
+        /// we need to distinguish between those two classes of operation so we might
+        /// be able to suspend a summation so multiplication or division can be done
+        /// first
+        /// </summary>
+        /// <returns></returns>
         private bool NextIsSummation()
         {
             return TokenStream.TokensAvailable && (NextIs(typeof(MinusToken)) || NextIs(typeof(PlusToken)));
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private bool NextIsNotSummation()
         {
             return TokenStream.TokensAvailable 
@@ -312,11 +372,18 @@ namespace Calculater_eXtreme
                    || NextIs(typeof(ModToken)) );
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private bool NextIsParamterExpr()
         {
             return TokenStream.TokensAvailable && NextIsOpeningBracket() ;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         private void CheckOpenBracket()
         {
             if (!NextIsOpeningBracket() || !TokenStream.TokensAvailable)
@@ -327,6 +394,9 @@ namespace Calculater_eXtreme
                     );
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         private void CheckCloseBracket()
         {
             if (!(NextIsClosingBracket()) || !TokenStream.TokensAvailable)
@@ -337,6 +407,10 @@ namespace Calculater_eXtreme
                     );
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private Token GetTermOperator()
         {
             var c = TokenStream.GetNext();
@@ -348,6 +422,10 @@ namespace Calculater_eXtreme
             throw new Exception("Expected term operand '+' or '-' but found" + c);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private Token GetFactorOperator()
         {
             var c = TokenStream.GetNext();
@@ -363,11 +441,19 @@ namespace Calculater_eXtreme
             throw new Exception("Expected factor operand '/' or '*' or '^' or '%' but found" + c);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private Token PeekNext()
         {
             return TokenStream.TokensAvailable ? TokenStream.PeekNext() : null;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private double GetNumber()
         {
             var next = TokenStream.GetNext();
